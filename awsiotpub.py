06b23f8b0411e77e6df61e7b9edcd2b899831add
Mikko Poutanen <mikko.poutanen@vincit.fi>
2016-08-12 12:54:14 +0300

    Add status report and register to publish

--- awsiotpub.py
+++ awsiotpub.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-# this source is part of my Hackster.io project:  https://www.hackster.io/mariocannistra/radio-astronomy-with-rtl-sdr-raspberrypi-and-amazon-aws-iot-45b617
+# this source is part of my Hackster.io project:
+# https://www.hackster.io/mariocannistra/radio-astronomy-with-rtl-sdr-raspberrypi-and-amazon-aws-iot-45b617
 
 # use this program to test the AWS IoT certificates received by the author
 # to participate to the spectrogram sharing initiative on AWS cloud
@@ -9,6 +10,8 @@
 # to test this program you have to run first its companion awsiotsub.py
 # that will subscribe and show all the messages sent by this program
 
+import sys
+import json
 import paho.mqtt.client as paho
 import ssl
 import string
@@ -48,8 +51,10 @@ mqttc.connect(awshost, awsport, keepalive=60)
 
 mqttc.loop_start()
 
+
 def randomstring(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
 
 def send_temperature():
     while True:
@@ -61,6 +66,7 @@ def send_temperature():
         else:
             print("waiting for connection...")
 
+
 def send_random_stuff():
     while True:
         sleep(3.0)
@@ -71,8 +77,57 @@ def send_random_stuff():
         else:
             print("waiting for connection...")
 
-t1 = threading.Thread(target=send_temperature)
-t1.start()
-t2 = threading.Thread(target=send_random_stuff)
-t2.start()
 
+def send_report():
+    is_sent = False
+    while not is_sent:
+        sleep(1.0)
+        if connflag:
+            data_dict = {
+                'ID': '000000000000',
+                'Temperature': 24.1,
+                'SetPoint': 25.5,
+                'ValvePosition': 50,
+                'BatteryLevel': 75.4,
+                'RSSI': -22.4
+            }
+
+            data_json = json.dumps(data_dict)
+            mqttc.publish("staging/report", data_json, qos=1)
+            print("msg sent to staging/report: %s" % data_json)
+            # Need to sleep here for a bit so that the script won't exit before the message has been sent
+            # sleep(3.0)
+            # is_sent = True
+        else:
+            print("waiting for connection...")
+
+
+def send_registration():
+    is_sent = False
+    while not is_sent:
+        sleep(1.0)
+        if connflag:
+            data_dict = {
+                'MACaddress': '123412341234',
+                'IPaddress': '123.123.23.34'
+            }
+            data_json = json.dumps(data_dict)
+            mqttc.publish("staging/register", data_json, qos=1)
+            print("msg sent to staging/register: %s" % data_json)
+            # Need to sleep here for a bit so that the script won't exit before the message has been sent
+            # sleep(3.0)
+            # is_sent = True
+        else:
+            print("waiting for connection...")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'register':
+        send_registration()
+    elif len(sys.argv) > 1 and sys.argv[1] == 'report':
+        send_report()
+    else:
+        t1 = threading.Thread(target=send_temperature)
+        t1.start()
+        t2 = threading.Thread(target=send_random_stuff)
+        t2.start()
--- awsiotsub.py
+++ awsiotsub.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-# this source is part of my Hackster.io project:  https://www.hackster.io/mariocannistra/radio-astronomy-with-rtl-sdr-raspberrypi-and-amazon-aws-iot-45b617
+# this source is part of my Hackster.io project:
+# https://www.hackster.io/mariocannistra/radio-astronomy-with-rtl-sdr-raspberrypi-and-amazon-aws-iot-45b617
 
 # use this program to test the AWS IoT certificates received by the author
 # to participate to the spectrogram sharing initiative on AWS cloud
@@ -9,8 +10,6 @@
 # awsiotpub.py using the AWS IoT hub
 
 import paho.mqtt.client as paho
-import os
-import socket
 import ssl
 import sys
 
@@ -20,23 +19,26 @@ if len(sys.argv) <= 1:
 else:
     topic_name = sys.argv[1]
 
+
 def on_connect(client, userdata, flags, rc):
-    print("Connection returned result: " + str(rc) )
+    print("Connection returned result: " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(topic_name , 1)
+    client.subscribe(topic_name, 1)
+
 
 def on_message(client, userdata, msg):
     print("topic: "+msg.topic)
     print("payload: "+str(msg.payload))
 
-#def on_log(client, userdata, level, msg):
-#    print(msg.topic+" "+str(msg.payload))
+
+# def on_log(client, userdata, level, msg):
+#     print(msg.topic+" "+str(msg.payload))
 
 mqttc = paho.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
-#mqttc.on_log = on_log
+# mqttc.on_log = on_log
 
 awshost = "data.iot.eu-west-1.amazonaws.com"
 awsport = 8883
@@ -46,7 +48,8 @@ caPath = "aws-iot-rootCA.crt"
 certPath = "cert.pem"
 keyPath = "privkey.pem"
 
-mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
+mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED,
+              tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
 
 mqttc.connect(awshost, awsport, keepalive=60)
 
